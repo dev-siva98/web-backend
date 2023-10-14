ccdc1ea319d79055fd6c68ab1f30750902bd88a9
Siva <sivaprasadtppba@gmail.com>
2023-10-14 12:34:03 +0530

    line removed

--- helpers/orderHelpers.js
+++ helpers/orderHelpers.js
@@ -6,7 +6,6 @@ require('dotenv').config()
 
 var instance = new Razorpay({ key_id: process.env.RZP_KEY_ID, key_secret: process.env.RZP_KEY_SECRET })
 
-
 module.exports = {
 
     getAllOrders: () => {
